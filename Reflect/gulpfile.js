298ef3b20c8fbf6f1ad1bdd808dc6e3854f59cd7
Carl Eriksson <carl.eriksson@funka.com>
2016-12-03 14:41:19 +0100

    installed foundation

--- Reflect/gulpfile.js
+++ Reflect/gulpfile.js
@@ -1,3 +1,29 @@
-﻿var gulp = require("gulp");
+﻿var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    concat = require("gulp-concat");
+
+var paths = {
+    webroot: "./Features/Home/Static/"
+};
+
+
+
+paths.scss = paths.webroot + "home.scss";
+paths.css = paths.webroot;
+
+
+gulp.task("watch", function () {
+    //gulp.watch(paths.js, ['min:js', browserSync.reload]);
+    console.log(paths.scss);
+    gulp.watch(paths.scss, ['sass']);
+});
+
+gulp.task('sass', function () {
+    return gulp.src(paths.scss)
+        .pipe(sass())
+        .pipe(concat("foundation.css"))
+        .pipe(gulp.dest(paths.css));
+});
+
 
 gulp.task("default", ["watch"]);
