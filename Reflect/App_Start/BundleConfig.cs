4addd16ad3395f9e2c2480744d8adb31c0bc4583
Carl Eriksson <carl.eriksson@funka.com>
2016-12-03 13:41:17 +0100

    Added mandatory files

---
+++ Reflect/App_Start/BundleConfig.cs
@@ -0,0 +1,7 @@
+﻿using System.Web.Optimization;
+namespace Reflect
+{
+    public class BundleConfig {
+        public static void RegisterBundles(BundleCollection bundles) {}
+    }
+}
---
+++ Reflect/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Reflect.Main.ViewEngines;
+
+namespace Reflect
+{
+    public class Startup : HttpApplication
+    {
+        protected void Application_Start() {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGloablFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ViewEngines.Engines.Remove(ViewEngines.Engines.OfType<WebFormViewEngine>().First());
+            ViewEngines.Engines.Remove(ViewEngines.Engines.OfType<RazorViewEngine>().First());
+            //ViewEngines.Engines.Add(new LayoutViewEngine());
+            ViewEngines.Engines.Add(new FeaturesLayoutViewEngine());
+        }
+    }
+}
