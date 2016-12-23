743e805347187229e9532c4af01a78131a4a719c
Carl Eriksson <carl.eriksson@funka.com>
2016-12-23 21:03:28 +0100

    gulp inject

--- Reflect/gulpfile.js
+++ Reflect/gulpfile.js
@@ -5,7 +5,7 @@
 
 var paths = {
     webroot: "./Features/Home/Static/",
-    scripts: ["./Features/**/Static/*.js", "./Main/Static/js/*.js"],
+    js: ["./Features/**/Static/*.js", "./Main/Static/js/*.js"],
     scss: ["./Features/**/Static/*.scss", "./Main/Static/scss/*.scss"],
     css: ["./Main/Static/css/*.css"],
     cssoutput: ["./Main/Static/css/"]
@@ -15,17 +15,21 @@ var paths = {
 //paths.css = paths.webroot;
 
 gulp.task("index", function () {
+      //return gulp.src('./Features/Layouts/beer.cshtml')
+      //      .pipe(inject(gulp.src(paths.js, { read: false })))
+      //      .pipe(gulp.dest('./Features'));
     return gulp.src("./Features/Layouts/beer.cshtml")
-          .pipe(inject(gulp.src(paths.scripts, { read: false }), { relative: false }))
+          .pipe(inject(gulp.src(paths.js, { read: false }), { relative: true }, { name: 'js' }))
           .pipe(gulp.dest("./Features"))
-          .pipe(inject(gulp.src(paths.css, { read: false }), { relatiive: true }))
+          .pipe(inject(gulp.src(paths.css, { read: false }), { relative: true }), { name: 'css' })
           .pipe(gulp.dest("./Features"));
+
 });
 gulp.task("watch", function () {
     //gulp.watch(paths.js, ['min:js', browserSync.reload]);
     console.log(paths.scss);
     gulp.watch(paths.scss, ['sass']);
-    gulp.watch([paths.scripts, paths.css], ["index"]);
+    gulp.watch([paths.js, paths.css], ["index"]);
 });
  
 gulp.task('sass', function () {
