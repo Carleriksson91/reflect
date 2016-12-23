288bd72ed82ed7d9718750ede70f3edd0e648203
Carl Eriksson <carl.eriksson@funka.com>
2016-12-23 19:18:50 +0100

    improved ask function

--- Reflect/Features/Home/HomeController.cs
+++ Reflect/Features/Home/HomeController.cs
@@ -5,41 +5,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Reflect.Context.Interfaces;
 
 namespace Reflect.Features.Home
-{ 
+{
     public class HomeController : Controller
     {
-        public ActionResult Index() {
-         //var model = new HomeViewModel(GetQuestions());
+        private readonly IQuestionRepository questionRepository;
+
+        public HomeController(IQuestionRepository quesionRepository)
+        {
+            this.questionRepository = quesionRepository;
+        }
+
+        public ActionResult Index()
+        {
+            //var model = new HomeViewModel(GetQuestions());
             //return View(model);
             return View();
         }
-      [HttpPost]
-      public ActionResult Ask(string title, string content) {
-         using(var context = new AzureContext()) {
-            context.Questions.Add(new Context.Entites.Question() {
-               Title = title,
-               Content = content,
-               Id = 0,
-               Date = DateTime.Now
+        [HttpPost]
+        public ActionResult Ask(string title, string content)
+        {
+            questionRepository.Add(new Question()
+            {
+                Title = title,
+                Content = content,
+                Id = 0,
+                Date = DateTime.Now
             });
+            return RedirectToAction("Index");
+        }
 
-            context.SaveChanges();
-         }
+        [HttpGet]
+        public IEnumerable<Question> GetQuestions()
+        {
+            var questions = new List<Question>();
+            using (var context = new AzureContext())
+            {
+                questions.AddRange(context.Questions.OrderByDescending(x => x.Date));
+            }
 
-         return RedirectToAction("Index");
-
-      }
-
-      [HttpGet]
-      public IEnumerable<Question> GetQuestions() {
-         var questions = new List<Question>();
-         using(var context = new AzureContext()) {
-            questions.AddRange(context.Questions.OrderByDescending(x => x.Date));
-         }
-
-         return questions;
-      }
-      }
+            return questions;
+        }
+    }
 }
