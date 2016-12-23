a59f101296e795a29bc833135a9ccf7c48dd97f6
Carl Eriksson <carl.eriksson@funka.com>
2016-12-23 19:17:12 +0100

    added repo pattern

---
+++ Reflect/Context/Repositories/Repository.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using Reflect.Context.Interfaces;
+
+namespace Reflect.Context.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected DbContext _context;
+
+        public Repository(DbContext context)
+        {
+            _context = context;
+        }
+
+
+        public void Add(TEntity entity)
+        {
+            _context.Set<TEntity>().Add(entity);
+        }
+
+        public void AddRange(IEnumerable<TEntity> entities)
+        {
+            _context.Set<TEntity>().AddRange(entities);
+        }
+
+        //    public int SaveChanges(DbContext context, EntityEntry<TEntity> entity)
+        //    {
+        //        var entities = context.ChangeTracker
+        //.Entries()
+        //.Where(x => x.State == EntityState.Modified || x.State == EntityState.Added && x.Entity != null).Select(x => x.Entity).ToList();
+        //        return 0;
+        //    }
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _context.Set<TEntity>().Where(predicate);
+        }
+
+        public TEntity Get(Guid id)
+        {
+            return _context.Set<TEntity>().Find(id);
+        }
+
+        public void Update(TEntity entity) {
+            //Todo: Implement this, when needed
+        }
+
+        public TEntity Get(int id)
+        {
+            return _context.Set<TEntity>().Find(id);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _context.Set<TEntity>().ToList();
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            try
+            {
+                _context.Set<TEntity>().RemoveRange(entities);
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
